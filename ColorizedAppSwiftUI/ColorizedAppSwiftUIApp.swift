aaf6802b7e299e533ca2722164768eb889f9f9c9
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-04-24 16:29:49 +0800

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by Dmitrii Melnikov on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
